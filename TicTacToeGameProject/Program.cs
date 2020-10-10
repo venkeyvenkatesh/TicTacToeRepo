3dfc0ec12e50ae3acd833a4ef389f86a1ffef87b
venkey <venkeyvenkatesh885@gmail.com>
2020-10-10 14:50:47 +0530

    [venkey] Add. added TicTacToeGame class and created CreateBoard method to create the game board

--- TicTacToeGameProject/Program.cs
+++ TicTacToeGameProject/Program.cs
@@ -7,6 +7,9 @@ namespace TicTacToeGameProject
         static void Main(string[] args)
         {
             Console.WriteLine("Hello!! Welcome to Tic Tac Toe game!");
+
+            TicTacToeGame game = new TicTacToeGame();
+            game.CreateBoard();
         }
     }
 }
---
+++ TicTacToeGameProject/TicTacToeGame.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToeGameProject
+{
+    class TicTacToeGame
+    {
+        //creating board char array
+        public char[] board;
+
+        //method to create game Board
+        public void CreateBoard()
+        {
+            board = new char[10];
+            for (int i=1;i<10;i++)
+            {
+                board[i] = Convert.ToChar('0');
+            }
+        }
+    }
+}
