8526d0ee53c4054536169108c6a88abeed571726
venkey <venkeyvenkatesh885@gmail.com>
2020-10-10 15:49:22 +0530

    [venkey]Add. Added showBoard method to display the board

--- TicTacToeGameProject/TicTacToeGame.cs
+++ TicTacToeGameProject/TicTacToeGame.cs
@@ -8,48 +8,71 @@ namespace TicTacToeGameProject
     class TicTacToeGame
     {
         //creating board char array
-        public char[] board=new char[10];
+        public char[] board = new char[10];
         public char playerLetter, computerLetter;
 
+
+        //starting the game
         public void startGame()
         {
             CreateBoard();
             ChooseLetter();
             Console.WriteLine("Your mark is :" + playerLetter);
             Console.WriteLine("Computer mark is :" + computerLetter);
-        }
-        //method to create game Board
-        public void CreateBoard()
-        {
-           
-            for (int i=1;i<10;i++)
-            {
-                board[i] = Convert.ToChar('0');
-            }
+
+            showBoard();
         }
 
+
+        //UC1 method to create game Board
+        public void CreateBoard()
+        {
+
+            for (int i = 1; i < 10; i++)
+            {
+                board[i] = ' ';
+            }
+            Console.WriteLine("Game board has been initialised.....");
+        }
+
+
+        //UC2 method to choose the letter by player
         public void ChooseLetter()
         {
             while (true)
             {
                 Console.WriteLine("Choose either X or O");
 
-                playerLetter = Convert.ToChar(Console.ReadLine());
+                playerLetter = char.ToUpper(Convert.ToChar(Console.ReadLine()));
 
                 if (playerLetter.Equals('X') || playerLetter.Equals('O'))
                 {
                     break;
                 }
             }
+
             if (playerLetter.Equals('X'))
             {
-               computerLetter = 'O';
+                computerLetter = 'O';
             }
             else
             {
                 computerLetter = 'X';
             }
 
+        }
+
+        //UC3 Displaying the game board
+        public void showBoard()
+        {
+            for(int i=1;i<10;i++)
+            {
+                Console.Write(board[i] + "\t");
+                if(i==3||i==6)
+                {
+                    Console.Write("\n");
+                }
+            }
         }
 
     }
